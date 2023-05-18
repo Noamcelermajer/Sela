3d51e0d9f82d1dedd66697975d39e48941303aa7
noamcelermajer <noam.celermajer@gmail.com>
2023-05-18 14:06:47 +0300

    finshed 2 assignment

---
+++ Algorithms and Complexity (using C#)/Excersice 12/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine(multiples(9876));
+    }
+
+    public static int multiples(int number)
+    {
+        if(number < 10)
+        {
+            return number;
+        }
+        return number % 10 * multiples(number / 10);
+    }
+}
