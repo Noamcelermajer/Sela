85d1647a536eb0ba2e12a1e016a3e57427ce42f7
Noam Celermajer <skyfrye@pop-os.localdomain>
2023-05-27 12:59:06 +0300

    s

--- Algorithms and Complexity (using C#)/Excersice 15/Board.cs
+++ Algorithms and Complexity (using C#)/Excersice 15/Board.cs
@@ -1,11 +1,66 @@
-namespace QueenProblem
+class Board
 {
-    class Board
+    private int[,] grid;
+
+    public Board(int size)
     {
-        private int[,] _board; 
-        public Board()
+        Size = size;
+        grid = new int[Size, Size];
+    }
+
+    public int Size { get; }
+
+    public void PlaceQueen(int row, int col)
+    {
+        grid[row, col] = 1;
+    }
+
+    public void RemoveQueen(int row, int col)
+    {
+        grid[row, col] = 0;
+    }
+
+    public bool IsSafe(int row, int col)
+    {
+        // Check the row
+        for (int i = 0; i < col; i++)
         {
-            _board = new int[8,8];
+            if (grid[row, i] == 1)
+                return false;
         }
 
+        // Check the upper diagonal
+        int x = row, y = col;
+        while (x >= 0 && y >= 0)
+        {
+            if (grid[x, y] == 1)
+                return false;
+            x--;
+            y--;
+        }
+
+        // Check the lower diagonal
+        x = row; y = col;
+        while (x < Size && y >= 0)
+        {
+            if (grid[x, y] == 1)
+                return false;
+            x++;
+            y--;
+        }
+
+        return true;
+    }
+
+    public void PrintBoard()
+    {
+        for (int row = 0; row < Size; row++)
+        {
+            for (int col = 0; col < Size; col++)
+            {
+                Console.Write(grid[row, col] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
 }
--- Algorithms and Complexity (using C#)/Excersice 15/Program.cs
+++ Algorithms and Complexity (using C#)/Excersice 15/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static int N = 8;
+
+    static bool SolveNQueens(Board board, int col)
+    {
+        // Base case: all queens are placed
+        if (col >= N)
+            return true;
+
+        for (int row = 0; row < N; row++)
+        {
+            if (board.IsSafe(row, col))
+            {
+                // Place the queen at (row, col)
+                board.PlaceQueen(row, col);
+
+                // Recursively place queens in the remaining columns
+                if (SolveNQueens(board, col + 1))
+                    return true;
+
+                // If placing the queen at (row, col) doesn't lead to a solution,
+                // backtrack and remove the queen from the cell.
+                board.RemoveQueen(row, col);
+            }
+        }
+
+        return false;
+    }
+
+    static void Main()
+    {
+        Board board = new Board(N);
+
+        if (SolveNQueens(board, 0))
+        {
+            board.PrintBoard();
+        }
+        else
+        {
+            Console.WriteLine("No solution exists.");
+        }
+    }
+}
